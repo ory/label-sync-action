1f31f88476f60e38231f034c60864f4edd62244c
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-08-20 15:34:13 +0200

    feat: implement github label sync action

---
+++ index.js
@@ -0,0 +1,40 @@
+const github = require('@actions/github')
+const core = require('@actions/core')
+const fs = require('fs')
+
+const sync = require('github-label-sync')
+const token = core.getInput('GITHUB_TOKEN') || process.env.GITHUB_TOKEN
+const dry = core.getInput('dry') !== 'false'
+const forced = core.getInput('forced') === 'true'
+const {repo, owner} = github.context.repo
+
+const labels = require('./default-labels.json')
+
+let config = core.getInput('config') || '.github/labels.json'
+
+if (fs.existsSync(config)) {
+  const extra = JSON.parse(fs.readFileSync(config).toString())
+  labels.push(...extra)
+}
+
+const milestones = new Array(10).fill(0).map((v, k) => ({
+  "name": `corp/m${k + 2}`,
+  "description": `Up for M${k + 2} at Ory Corp.`,
+  "color": "6274F3"
+}))
+
+labels.push(...milestones)
+
+sync({
+  accessToken: token,
+  repo: `${owner}/${repo}`,
+  dryRun: dry,
+  allowAddedLabels: !(forced),
+  labels,
+})
+  .then((diff) => {
+    console.log("Removing labels:", diff.filter(({type}) => type === 'added').map(({name}) => name).join(','))
+    console.log("Adding labels:", diff.filter(({type}) => type === 'missing').map(({name}) => name).join(','))
+    console.log("Changed labels:", diff.filter(({type}) => type === 'changed').map(({name}) => name).join(','))
+  })
+  .catch((err)=>console.error(JSON.stringify(err,null,2)))
---
+++ node_modules/github-label-sync/CHANGELOG.md
@@ -0,0 +1,31 @@
+
+# Changelog
+
+## 2.0.0 (2020-06-03)
+
+  * Drop support for node 4,5,6,7,8,9,10,11.
+
+## 1.3.0 (2017-08-15)
+
+  * Support URLs in the `--labels` flag
+
+## 1.2.1 (2017-05-16)
+
+  * Support repositories with a lot of labels
+
+## 1.2.0 (2016-04-25)
+
+  * Update the default label set
+    * Add "status: good starter issue"
+    * Add "type: breaking"
+    * Add "type: discussion"
+    * Add alias for "status: wontfix"
+
+## 1.1.0 (2016-03-15)
+
+  * Update the default label set
+    * Add alias for "status: help wanted"
+
+## 1.0.0 (2016-03-15)
+
+  * Initial release
